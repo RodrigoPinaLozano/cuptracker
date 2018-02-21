d7f02df34f836c63fbdc06b7ce5761ff74938289
Rodrigo Pina Lozano (CI/BDM-EU) <rodrigo.pina@de.bosch.com>
2018-02-21 12:43:20 +0100

    version with web-view

---
+++ cuptracker-app/app/app.js
@@ -0,0 +1,13 @@
+require("./bundle-config");
+const application = require("application");
+
+if (application.ios) {
+    GMSServices.provideAPIKey("AIzaSyBpCBZTRmUsRHqQgfDEaLprkqqYTxZjFQs");
+}
+
+application.start({ moduleName: "home/home-page" });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
